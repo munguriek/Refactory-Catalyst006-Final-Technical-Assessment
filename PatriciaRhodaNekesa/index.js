190d771db020c610a53434b1381ab28acd855a07
Kesa <nekepatri@gmail.com>
2020-12-04 11:59:57 +0300

    Successfully connected the database and app is running

---
+++ PatriciaRhodaNekesa/index.js
@@ -0,0 +1,38 @@
+//Import Dependencies
+const express = require("express"); 
+const mongoose = require("mongoose"); 
+const bodyParser = require("body-parser");
+const path = require("path");
+
+require("dotenv/config")
+
+
+const app = express();
+
+//  DB Connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.listen(3000, () => console.log("Listening on port 3000")); //Created a server and have it's listen on port 3000
+
+
